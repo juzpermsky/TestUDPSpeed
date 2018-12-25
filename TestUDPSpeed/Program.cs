e05bd38e4f1dfff77752042d05f700210ab41ede
Lukyanenko Evgeny <juz@hive.ru>
2018-12-25 11:50:40 +0500

    Тест отправки в цикле 10000 10-байтовых сообщений без задержки по UDP в main thread ~0.15 с С запущенным RawCap ~0.26 с

---
+++ TestUDPSpeed/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestUDPSpeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.100"), 5001));
+
+            var sample = new byte[10];
+            var receiver = new IPEndPoint(IPAddress.Parse("192.168.1.100"), 5000);
+            var i = 0;
+            var t1 = DateTime.Now;
+            while (i < 10000)
+            {
+                socket.SendTo(sample, receiver);
+                i++;
+            }
+
+            Console.WriteLine($"{i} samples sent in {DateTime.Now - t1}");
+        }
+    }
+}
